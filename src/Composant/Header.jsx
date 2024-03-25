a48536c3f3aba4e15bbe4f996b7ad2d270af2ac6
itimoreo <pol.lopezpro@gmail.com>
2024-03-25 15:11:41 +0100

    header 1.0.0

--- src/Composant/Header.jsx
+++ src/Composant/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ArgentBankLogo from '../assets/img/argentBankLogo.png';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
+
+const UserIcon = () => {
+    return (
+        <div style={{color:'#2c3e50'}}>
+            <FontAwesomeIcon icon={faUserCircle} />
+            <a href="#" style={{marginLeft: '10px',marginRight: '0.5rem', fontSize:'16px', textDecoration: 'none', color: 'inherit'}}>Sign-in</a>
+        </div>
+    );
+}
+
 
 const Header = () => {
     return (
-        <header>
-            <img src={ArgentBankLogo} alt="Logo de la banque" />
+        <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding:'5px 20px', fontWeight:'bold' }}>
+            <img src={ArgentBankLogo} alt="Logo de la banque" style={{ width: '200px' }} />
+            <UserIcon />
         </header>
     );
 };
