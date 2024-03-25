40e492ec89ed0552a88ac4d5fe1f33ef9b7946ba
itimoreo <pol.lopezpro@gmail.com>
2024-03-25 14:37:02 +0100

    header 0.01

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header__logo">
+          <h1 className="header__title">Argent Bank</h1>
+      </div>
       </header>
     </div>
   );
