d8ae3e9828299162b9aaab03d634864a1e6ba536
itimoreo <pol.lopezpro@gmail.com>
2024-04-12 14:47:51 +0200

    collapse qui marche + stylisation

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,51 +1,54 @@
-import React, { useState } from 'react';
-import { Collapse } from 'react-bootstrap';
-import {useLocation} from 'react-router-dom';
-import dashboardStyle from"../css/Dashboard.css";
-import editNameStyle from "../css/EditName.css";
+import React, { useState } from "react";
 import TransactionCollapsed from "./TransactionCollapsed";
-import classNames from 'classnames';
 
 const Transaction = () => {
-  const [open1, setOpen1] = useState(false);
-  const [open2, setOpen2] = useState(false);
-  const [open3, setOpen3] = useState(false);
-
-  const location = useLocation();
-
-  let cssClass = classNames({
-    [dashboardStyle]: location.pathname.includes("dashboard"),
-    [editNameStyle]: location.pathname.includes("edit"),
-  });
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
 
   return (
-    <div className={cssClass}>
-      <div className="bankAccount-container" style={{ cursor:'pointer', color: "#2c3e50" }} onClick={() => setOpen1(!open1)}>
+    <div>
+      <div
+        className="bankAccount-container"
+        style={{
+          cursor: "pointer",
+          color: "white",
+          backgroundColor: "#424242",
+        }}
+        onClick={() => setIsOpen1(!isOpen1)}
+      >
         <h3 className="bankAccount-title">Argent Bank Checking (x8349)</h3>
         <p className="bankAccount-amount">$2,082.79</p>
         <p className="bankAccount-description">Available Balance</p>
-        <Collapse in={open1}>
-          <div>
-          </div>
-        </Collapse>
+        {isOpen1 && <TransactionCollapsed />}
       </div>
-      <div className="bankAccount-container" style={{ cursor:'pointer', color: "#2c3e50" }} onClick={() => setOpen2(!open2)}>
+      <div
+        className="bankAccount-container"
+        style={{
+          cursor: "pointer",
+          color: "white",
+          backgroundColor: "#424242",
+        }}
+        onClick={() => setIsOpen2(!isOpen2)}
+      >
         <h3 className="bankAccount-title">Argent Bank Checking (x6712)</h3>
         <p className="bankAccount-amount">$10,928.42</p>
         <p className="bankAccount-description">Available Balance</p>
-        <Collapse in={open2}>
-          <div>
-          </div>
-        </Collapse>
+        {isOpen2 && <TransactionCollapsed />}
       </div>
-      <div className="bankAccount-container" style={{ cursor:'pointer', color: "#2c3e50" }} onClick={() => setOpen3(!open3)}>
+      <div
+        className="bankAccount-container"
+        style={{
+          cursor: "pointer",
+          color: "white",
+          backgroundColor: "#424242",
+        }}
+        onClick={() => setIsOpen3(!isOpen3)}
+      >
         <h3 className="bankAccount-title">Argent Bank Checking (x8349)</h3>
         <p className="bankAccount-amount">$184.30</p>
         <p className="bankAccount-description">Available Balance</p>
-        <Collapse in={open3}>
-          <div>
-          </div>
-        </Collapse>
+        {isOpen3 && <TransactionCollapsed />}
       </div>
     </div>
   );
--- src/components/TransactionCollapsed.jsx
+++ src/components/TransactionCollapsed.jsx
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
-import { Collapse } from "react-bootstrap";
 
 const Transaction = () => {
-  const [open1, setOpen1] = useState(false);
-  const [open2, setOpen2] = useState(false);
-  const [open3, setOpen3] = useState(false);
 
   return (
     <div>
-      <Collapse in={open1}>
         <div>
           <p>Casino = 10€</p>
         </div>
-      </Collapse>
     </div>
   );
 };
